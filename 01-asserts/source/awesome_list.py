9255c608a82c745b37878a4e02c153a4282ef0ca
Sergio Gallardo Sales <ikeinyyo@outlook.com>
2019-12-03 10:17:09 +0100

    Add asserts

---
+++ 01-asserts/source/awesome_list.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+
+def sum(numbers):
+    return reduce(lambda number, acc: acc + number, numbers)
+
+
+def bubbleSort(numbers):
+    sorted_list = numbers.copy()
+    n = len(sorted_list)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if sorted_list[j] > sorted_list[j+1]:
+                sorted_list[j], sorted_list[j +
+                                            1] = sorted_list[j+1], sorted_list[j]
+    return sorted_list
+
+
+def sum_lists(list1, list2):
+    if len(list1) != len(list2):
+        raise Exception("Same size is required to sum two lists")
+    sum_list = []
+    for i in range(len(list1)):
+        sum_list.append(list1[i] + list2[i])
+    return sum_list
+
+
+if __name__ == "__main__":
+    l = [1, 4, 3, 5, 2]
+    print(sum(l))
+    print(l)
+    print(sum_lists(l, [1]))
