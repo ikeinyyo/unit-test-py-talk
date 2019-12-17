0a845c72692ed08a44ea9f66a2fe488df999a454
Sergio Gallardo Sales <ikeinyyo@outlook.com>
2019-12-17 14:30:01 +0100

    Start bizum sample

--- 03-tdd/source/utils/bizum.py
+++ 03-tdd/source/utils/bizum.py
@@ -1,11 +1,2 @@
 def bizum(num):
-    if not isinstance(num, int):
-        raise Exception("num")
-
-    result = ""
-    if num % 3 == 0:
-        result += "Bi"
-    if num % 5 == 0:
-        result += "Zum"
-
-    return result if result else str(num)
+    pass
